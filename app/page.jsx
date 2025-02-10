3a2a1a8cc4ab45494d44596963a41300dd865187
rajiv <rajivdahal89@gmail.com>
2025-02-10 21:31:05 +0545

    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <section>
+      <div className={Styles.title}>
+        <h1 className="">Hello World</h1>
+      </div>
+    </section>
+  );
+}
